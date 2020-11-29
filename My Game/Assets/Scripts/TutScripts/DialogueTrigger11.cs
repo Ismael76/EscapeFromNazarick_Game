d1024ec766ffcc20ba9e4b272b0848008c16a568
Ismael <IsmaelAli79@Outlook.Com>
2020-11-29 03:11:37 +0000

    Completed Tutorial

---
+++ My Game/Assets/Scripts/TutScripts/DialogueTrigger11.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger11 : MonoBehaviour
+{
+    public GameObject panel9;
+    public GameObject panel7;
+    public GameObject panel8;
+    public GameObject panel6;
+    public GameObject panel5;
+    public GameObject panel4;
+    public GameObject panel3;
+    public GameObject panel2;
+    public GameObject panel1;
+    public GameObject panel10;
+    public GameObject panel11;
+    public GameObject panel12;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (panel12 == null)
+        {
+            return;
+        }
+        Destroy(panel9);
+        Destroy(panel11);
+        Destroy(panel10);
+        Destroy(panel8);
+        Destroy(panel7);
+        Destroy(panel6);
+        Destroy(panel5);
+        Destroy(panel4);
+        Destroy(panel3);
+        Destroy(panel2);
+        Destroy(panel1);
+        panel12.SetActive(true);
+
+
+    }
+}
---
+++ My Game/Assets/Scripts/TutScripts/DialogueTrigger9.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger9 : MonoBehaviour
+{
+    public GameObject panel9;
+    public GameObject panel7;
+    public GameObject panel8;
+    public GameObject panel6;
+    public GameObject panel5;
+    public GameObject panel4;
+    public GameObject panel3;
+    public GameObject panel2;
+    public GameObject panel1;
+    public GameObject panel10;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (panel10 == null) {
+            return;
+        }
+        Destroy(panel9);
+        Destroy(panel8);
+        Destroy(panel7);
+        Destroy(panel6);
+        Destroy(panel5);
+        Destroy(panel4);
+        Destroy(panel3);
+        Destroy(panel2);
+        Destroy(panel1);
+        panel10.SetActive(true);
+
+
+    }
+}
