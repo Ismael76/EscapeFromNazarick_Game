49de2a3ae651a79f7ce9902e2f7ce8ac60f8acf4
Ismael <IsmaelAli79@Outlook.Com>
2020-12-06 02:55:20 +0000

    Level 3 Fully Complete

---
+++ My Game/Assets/Scripts/GameplayScripts/NPCAttach.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAttach : MonoBehaviour
+{
+    public GameObject npc;
+
+    //Attachs The Player Position To The Moving Platform Object
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject == npc)
+        {
+            npc.transform.parent = this.transform;
+
+        }
+    }
+
+    //When The Player Moves Off The Moving Platform Object, They Are De-attached
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == npc)
+        {
+
+            npc.transform.parent = null;
+        }
+
+    }
+}
